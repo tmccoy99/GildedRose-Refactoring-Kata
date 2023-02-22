5beab9928ee8bd6301c12eeb5987542934caeaf7
Thomas McCoy <finnmccoy99@gmail.com>
2023-02-22 14:19:13 +0000

    Item quality decreasing before sellIn expires is now tested

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,30 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Shop, Item } = require('../src/gilded_rose');
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+describe('Gilded Rose', function () {
+  describe('updateQuality testing for non conjured items without special names', () => {
+    test('for items with 0 quality, updateQuality reduces sellIn by 1 and does not affect quality', () => {
+      const sellIn = Math.floor((Math.random() - 0.5) * 100);
+      const gildedRose = new Shop([new Item('foo', sellIn, 0)]);
+      gildedRose.updateQuality();
+      const updatedItem = gildedRose.items[0];
+      expect([
+        updatedItem.name,
+        updatedItem.sellIn,
+        updatedItem.quality,
+      ]).toEqual(['foo', sellIn - 1, 0]);
+    });
+
+    test('for items with 0 < quality < 50 and sellIn >= 0, both sellIn and quality are reduced by 1', () => {
+      const sellIn = Math.floor(Math.random() * 100);
+      const quality = Math.floor(1 + Math.random() * 50);
+      const gildedRose = new Shop([new Item('foo', sellIn, quality)]);
+      gildedRose.updateQuality();
+      const updatedItem = gildedRose.items[0];
+      expect([
+        updatedItem.name,
+        updatedItem.sellIn,
+        updatedItem.quality,
+      ]).toEqual(['foo', sellIn - 1, quality - 1]);
+    });
   });
 });

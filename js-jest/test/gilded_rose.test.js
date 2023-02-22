b95a507623c2837923438c56144e2ba4ef27c3fd
Thomas McCoy <finnmccoy99@gmail.com>
2023-02-22 14:32:55 +0000

    Added test for quality reduction when sellIn has expired and quality >= 2

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -16,7 +16,7 @@ describe('Gilded Rose', function () {
 
     test('for items with 0 < quality < 50 and sellIn >= 0, both sellIn and quality are reduced by 1', () => {
       const sellIn = Math.floor(Math.random() * 100);
-      const quality = Math.floor(1 + Math.random() * 50);
+      const quality = Math.floor(1 + Math.random() * 49);
       const gildedRose = new Shop([new Item('foo', sellIn, quality)]);
       gildedRose.updateQuality();
       const updatedItem = gildedRose.items[0];
@@ -25,6 +25,19 @@ describe('Gilded Rose', function () {
         updatedItem.sellIn,
         updatedItem.quality,
       ]).toEqual(['foo', sellIn - 1, quality - 1]);
+    });
+
+    test('for items with 1 < quality < 50 and sellIn < 0, sellIn in reduced by 1 and quality is reduced by 2', () => {
+      const sellIn = -1 - Math.floor(Math.random() * 50);
+      const quality = Math.floor(2 + Math.random() * 48);
+      const gildedRose = new Shop([new Item('foo', sellIn, quality)]);
+      gildedRose.updateQuality();
+      const updatedItem = gildedRose.items[0];
+      expect([
+        updatedItem.name,
+        updatedItem.sellIn,
+        updatedItem.quality,
+      ]).toEqual(['foo', sellIn - 1, quality - 2]);
     });
   });
 });

887ab124f23d7bf320585a24bd72a1a23b2e9ed5
Thomas McCoy <finnmccoy99@gmail.com>
2023-02-24 11:32:05 +0000

    Refactored if else block to a switch block

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -12,30 +12,31 @@ class Shop {
   }
   updateQuality() {
     this.items.forEach((item) => {
-      if (item.name === 'Sulfuras, Hand of Ragnaros') {
-        return;
-      } else if (item.name === 'Aged Brie') {
-        const increment = item.sellIn <= 0 ? 2 : 1;
-        item.quality = Math.min(50, item.quality + increment);
-        item.sellIn += -1;
-        return;
-      } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
-        const increment =
-          item.sellIn >= 11
-            ? 1
-            : item.sellIn >= 6
-            ? 2
-            : item.sellIn >= 1
-            ? 3
-            : -item.quality;
-        item.quality = Math.min(50, item.quality + increment);
-        item.sellIn += -1;
-        return;
-      } else {
-        const increment = item.sellIn <= 0 ? -2 : -1;
-        item.quality = Math.max(0, item.quality + increment);
-        item.sellIn += -1;
-        return;
+      let increment;
+      switch (item.name) {
+        case 'Sulfuras, Hand of Ragnaros':
+          return;
+        case 'Aged Brie':
+          increment = item.sellIn <= 0 ? 2 : 1;
+          item.quality = Math.min(50, item.quality + increment);
+          item.sellIn += -1;
+          break;
+        case 'Backstage passes to a TAFKAL80ETC concert':
+          increment =
+            item.sellIn >= 11
+              ? 1
+              : item.sellIn >= 6
+              ? 2
+              : item.sellIn >= 1
+              ? 3
+              : -item.quality;
+          item.quality = Math.min(50, item.quality + increment);
+          item.sellIn += -1;
+          break;
+        default:
+          increment = item.sellIn <= 0 ? -2 : -1;
+          item.quality = Math.max(0, item.quality + increment);
+          item.sellIn += -1;
       }
     });
 
